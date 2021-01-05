f4052c5f3292c9dad1c9e2652ab37d25450cc79d
Pranshu Shukla <55992439+Pranshu-S@users.noreply.github.com>
2021-01-05 11:47:11 +0400

    Completed Model
    PID Tuning Left

---
+++ make_desired.m
@@ -0,0 +1,8 @@
+desired_X = 0;
+desired_Y = 0;
+desired_Z = 5;
+desired_phi = 0;
+desired_theta = 0;
+desired_gamma = 0;
+
+save desired.mat desired_X desired_Y desired_Z desired_phi desired_theta desired_gamma;
